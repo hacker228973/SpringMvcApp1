9a3d92465798748a800405fb8ca906e1bd7d7ae1
hacker228973 <kodiy9@gmail.com>
2022-08-16 17:13:43 +0200

    FINAL

--- src/main/java/com/hillel/dao/PersonDAO.java
+++ src/main/java/com/hillel/dao/PersonDAO.java
@@ -1,33 +1,40 @@
 package com.hillel.dao;
 
 import com.hillel.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
-
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
     }
-    public List<Person> index(){
-        return people;
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
     public Person show(int id){
-        return people.stream().filter(person-> person.getId()==id).findAny().orElse(null);
+//        return people.stream().filter(person-> person.getId()==id).findAny().orElse(null);
+
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id},new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
 
     public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(),person.getAge(),person.getEmail());
+    }
+    public void update(int id,Person updatePerson){
+        jdbcTemplate.update("UPDATE Person SET name=?,age=?,email=? WHERE id=?",updatePerson.getName(),updatePerson.getAge(),updatePerson.getEmail(),id);
+    }
+    public void delete(int id){
+jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
 }
 
